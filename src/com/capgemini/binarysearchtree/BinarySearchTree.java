a609885e510c7a5be57c15146bee68520af2a7f0
akshat1032 <akshat.30oct@gmail.com>
2020-10-12 22:22:57 +0530

    Added : Welcome message added

---
+++ src/com/capgemini/binarysearchtree/BinarySearchTree.java
@@ -0,0 +1,9 @@
+package com.capgemini.binarysearchtree;
+
+public class BinarySearchTree {
+	public static void main(String[] args) {
+		
+		// Wlecome message added
+		System.out.println("Welcome to Binary Search Tree Program");
+	}
+}
