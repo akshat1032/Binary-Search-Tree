1f9376456bdbf386001901cd9c85c36ae37ccf50
akshat1032 <akshat.30oct@gmail.com>
2020-10-12 23:37:15 +0530

    Added : Create a binary search and check for addition of all elements using size method added

--- src/com/capgemini/binarysearchtree/BinarySearchTree.java
+++ src/com/capgemini/binarysearchtree/BinarySearchTree.java
@@ -24,16 +24,39 @@ public class BinarySearchTree<K extends Comparable<K>> {
 		return current;
 	}
 	
-	//Printing the tree, root node first, followed by left node and then right node
-	private void printTree() {
-		System.out.println(this.root.key+" "+this.root.left.key+" "+this.root.right.key);
+	//Function to get size which internally calls recursive function
+	public int getSize() {
+		return this.getSizeRecursively(root);
 	}
+	
+	//Recursive function to get size
+	private int getSizeRecursively(BinaryNode<K> current) {
+		return current == null?0:1+this.getSizeRecursively(current.left)+this.getSizeRecursively(current.right);
+	}
+
+	
 	public static void main(String[] args) {
 		
 		BinarySearchTree binaryTree = new BinarySearchTree<>();
 		binaryTree.add(56);
 		binaryTree.add(30);
 		binaryTree.add(70);
-		binaryTree.printTree();
+		binaryTree.add(22);
+		binaryTree.add(40);
+		binaryTree.add(60);
+		binaryTree.add(95);
+		binaryTree.add(11);
+		binaryTree.add(65);
+		binaryTree.add(3);
+		binaryTree.add(16);
+		binaryTree.add(63);
+		binaryTree.add(67);
+		
+		//Checking if all elements are added to the tree using getSize() function
+		if (binaryTree.getSize() == 13) {
+			System.out.println("All elements added to the binary tree");
+		}
+		else
+			System.out.println("All elements were not added to the binary tree");
 	}
 }
